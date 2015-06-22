81b8c8c80efe3cdfe164ccf167260aa6a70240a4
Elliot Saba <staticfloat@gmail.com>
2015-06-22 06:56:57 -0700

    More debugging info on why our keychain unlocking fails so often!

--- templates/unlock_keychain.sh
+++ templates/unlock_keychain.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-security -v unlock-keychain -p "{{osx_keychain_password}}" ~/xcode.keychain && echo Unlocked
+security unlock-keychain -p "{{osx_keychain_password}}" ~/xcode.keychain && echo Unlocked
+security show-keychain-info ~/xcode.keychain
+security find-identity ~/xcode.keychain
